592ce61a8bdf88399692102c6a27f7dbf2180f7d
Velidoss <velidoss11@gmail.com>
2020-10-04 09:05:29 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!Array.isArray(array)){
+        return  0
+    }else{
+        return array.reduce((acc, el)=>{
+            if(el < acc){
+                acc = el
+            }
+            return acc
+        }, 0)
+    }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!Array.isArray(array)){
+        return  0
+    }else {
+        return array.reduce((acc, el) => {
+            if (el > acc) {
+                acc = el
+            }
+            return acc
+        }, 0)
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!Array.isArray(array)){
+        return  0
+    }else{
+        const sum =  array.reduce((acc, el)=>{
+            return acc+el
+        }, 0)
+        const res = parseFloat(parseFloat(sum)/array.length)
+        if(isNaN(res)){
+            return 0
+        }else {
+            return res
+        }
+    }
+
 }
